221de28d2b3a13c55f6c3fa47b7b8cce59084171
José Carlos <j.ccarlos.developer.roots@gmail.com>
2020-09-09 14:04:19 -0300

    Arquivo insert, com o código sql gerado corretamente, tentei apontar o caminho da pasta gerador_sql para dar um require_relative mas não foi possível.

---
+++ comandos/insert.rb
@@ -0,0 +1,11 @@
+puts "Digite o seu nome, para adicionar uma tabela com os dados."
+dados = gets.chomp
+
+puts "Gerando a tabela......"
+puts "CREATE TABLE dados (
+        nome VARCHAR(30),
+        idade INT,
+        cidade VARCHAR(20)
+        );"        
+
+
